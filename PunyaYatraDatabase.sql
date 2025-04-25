b0afd3d907d738823693551c111c1fc549cdda4e
SamarthKharade <kharadesamarth64@gmail.com>
2025-04-25 17:14:20 +0530

    Database file updated

--- PunyaYatraDatabase.sql
+++ PunyaYatraDatabase.sql
@@ -17,11 +17,21 @@ CREATE TABLE users (
 
 
 select * from users;
+ 
 
-
-create table restaurant_booking(name varchar(40) ,date_of_booking date,time_of_booking time,total_people int,total_table int);
+create table restaurant_booking(name varchar(40) ,restaurant_name varchar(50),restaurant_location varchar(50),email varchar(30),date_of_booking date,time_of_booking time,total_people int,total_table int);
 
 select * from restaurant_booking;
 
 
 
+create table hotel_booking(name varchar(40),
+hotel_name varchar(50),hotel_location varchar(50),
+email varchar(30),
+room_type varchar(20),
+ac_type varchar(10),
+total_people int,total_room int,
+reporting_date date,reporting_time time,
+exit_date date,exit_time time);
+
+select * from hotel_booking;
